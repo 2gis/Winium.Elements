469f6f26d78f95923d5f0d5c1c00832d40dcd30c
Golovin Gleb <g.golovin@2gis.ru>
2015-08-11 17:20:58 +0600

    Add powershell module for pack nuget package. Add prepare release script

---
+++ scripts/modules/nuget.psm1
@@ -0,0 +1,58 @@
+﻿Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+#------------------------------
+
+Function Invoke-NuGetPack()
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True)]
+        [string]$packTarget,
+        
+        [Parameter(Mandatory=$True)]
+        [string]$configuration,
+
+        [string]$outputDirectory = '.'
+    )
+    
+    $nugetPath = Get-NuGetPath
+    $arguments = @(
+        'pack'
+        $packTarget
+        '-IncludeReferencedProjects'
+        '-Properties'
+        "Configuration=$configuration"
+        '-OutputDirectory'
+        $outputDirectory
+    )
+
+    Write-Output '---> Run NuGet pack.'
+    Write-Verbose "NuGet path: $nugetPath"
+    Write-Verbose "NuGet arguments: $arguments"
+    & $nugetPath $arguments
+    if ($LASTEXITCODE -ne 0)
+    {
+        throw "NuGet pack failed with exit code $LASTEXITCODE"
+    }
+    
+    Write-Output '---> NuGet pack succeeded.'
+}
+
+Function Get-NuGetPath()
+{
+    if ($env:NUGET)
+    {
+        return $env:NUGET
+    }
+    
+    Write-Verbose 'Environment variable NUGET not set'
+    if (Get-Command 'nuget.exe' -ErrorAction SilentlyContinue) 
+    {
+        return 'nuget'
+    }
+    
+    Write-Verbose 'Unable to find nuget.exe in your PATH'
+    throw 'Unable to find path to nuget.exe. See more with -Verbose'
+}
+
+Export-ModuleMember -Function Invoke-NuGetPack
---
+++ scripts/prepareRelease.ps1
@@ -0,0 +1,22 @@
+﻿Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+#------------------------------
+
+Function CleanDirectory($releaseDir)
+{
+    if (Test-Path $releaseDir)
+    {
+        Remove-Item $releaseDir -Force -Recurse
+    }
+
+    New-Item -ItemType directory -Path $releaseDir | Out-Null
+}
+
+$releaseDir = Join-Path $PSScriptRoot '../Release'
+CleanDirectory $releaseDir
+
+Import-Module '.\ci.ps1'
+Import-Module '.\modules\nuget.psm1'
+
+$project = Join-Path $solutionDir 'Winium.Elements.Desktop\Winium.Elements.Desktop.csproj'
+Invoke-NuGetPack $project $configuration $releaseDir -Verbose
