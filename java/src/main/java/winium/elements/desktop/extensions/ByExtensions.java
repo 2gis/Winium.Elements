bc77fe8b1deceb7b6f76311286831dadc3c4605c
zebraxxl <zebraxxl@mail.ru>
2015-09-10 13:23:07 +0300

    ByExtensions ended

--- java/src/main/java/winium/elements/desktop/extensions/ByExtensions.java
+++ java/src/main/java/winium/elements/desktop/extensions/ByExtensions.java
@@ -2,16 +2,21 @@ package winium.elements.desktop.extensions;
 
 import org.openqa.selenium.By;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ByExtensions {
     private ByExtensions() { }
 
-    private static final String descriptionRegexp = "\\.(.*)\\: (.*)";
+    private static Pattern descriptionRegexp = Pattern.compile("\\.(.*): (.*)");
 
     public static Object getStrategy(By by) {
-        return null;
+        Matcher m = descriptionRegexp.matcher(by.toString());
+        return m.group(1).replaceAll("([A-Z])", " $1").split("\\[")[0].trim().toLowerCase();
     }
 
     public static Object getValue(By by) {
-        return null;
+        Matcher m = descriptionRegexp.matcher(by.toString());
+        return m.group(2);
     }
 }
