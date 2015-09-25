e36fa46498478d3b28e65584b0ceecc80c4321d7
Golovin Gleb <g.golovin@2gis.ru>
2015-09-25 15:16:29 +0600

    Add common ci script

---
+++ scripts/ci.ps1
@@ -0,0 +1,10 @@
+﻿Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+#------------------------------
+
+$root = $PWD
+
+# .NET
+cd ..\dotnet\scripts
+& .\ci.ps1
+Copy-Item nunit-result.xml -Destination $root
